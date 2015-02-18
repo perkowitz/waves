e9cdd1ae8c35548ce1679f4146598e80bc74673d
perkowitz <mike@perkowitz.net>
2015-02-17 22:44:07 -0800

    Simplified MultiProcessor args, added a bunch of processes 2

--- src/main/java/net/perkowitz/waves/MultiProcessor.java
+++ src/main/java/net/perkowitz/waves/MultiProcessor.java
@@ -1,5 +1,6 @@
 package net.perkowitz.waves;
 
+import com.google.common.collect.Lists;
 import org.codehaus.jackson.map.ObjectMapper;
 
 import java.io.File;
@@ -21,34 +22,59 @@ public class MultiProcessor {
 
     public static void main(String args[]) throws IOException {
 
-        if (args.length < 6) {
-            System.out.println("Usage: Process <process file> <low note> <high note> <skip> <outdir> <filename>");
+        if (args.length < 5) {
+            System.out.println("Usage: Process <process file/dir> <audio dir> <low note> <high note> <skip>");
             return;
         }
 
-        String processFile = args[0];
-        Integer lowNote = new Integer(args[1]);
-        Integer highNote = new Integer(args[2]);
-        Integer skip = new Integer(args[3]);
-        String outdirString = args[4];
-        String filename = args[5];
+        File processIn = new File(args[0]);
+        String outdirString = args[1];
+        Integer lowNote = new Integer(args[2]);
+        Integer highNote = new Integer(args[3]);
+        Integer skip = new Integer(args[4]);
 
         File outdir = new File(outdirString);
         if (!outdir.exists()) {
             outdir.mkdirs();
         }
 
-        Process process = Process.parseFromFile(processFile);
-        for (int note=lowNote; note <= highNote; note+=skip) {
-            System.out.printf("Rendering note: %d\n", note);
-            String outFile = outdirString + "/" + filename + "_" + note;
-            Wave wave = process.apply(note);
-            StdAudio.save(outFile + WAV_FILE_SUFFIX, wave.toDoubleArray());
+        List<File> inFiles = Lists.newArrayList();
+        if (processIn.isDirectory()) {
+            for (File file : processIn.listFiles()) {
+                inFiles.add(file);
+            }
+        } else {
+            inFiles.add(processIn);
         }
 
+        for (File in : inFiles) {
+
+            String filestem = in.getName();
+            if (filestem.endsWith(".json")) {
+                filestem = filestem.substring(0,filestem.length()-5);
+            }
+
+            String outStem = outdirString + "/" + filestem;
+
+            processFile(in.toString(), outStem, lowNote, highNote, skip);
+
+        }
 
     }
 
+    private static void processFile(String processFile, String outStem, int lowNote, int highNote, int skip) {
+
+        System.out.printf("Processing %s to %s\n", processFile, outStem);
+        Process process = Process.parseFromFile(processFile);
+        for (int note=lowNote; note <= highNote; note+=skip) {
+            System.out.printf("Rendering note: %d\n", note);
+            Wave wave = process.apply(note);
+
+            String outFile = outStem + "_" + note + WAV_FILE_SUFFIX;
+            StdAudio.save(outFile, wave.toDoubleArray());
+        }
+
+    }
 
 
 }
